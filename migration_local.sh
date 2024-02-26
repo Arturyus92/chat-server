fba9bbfc5d02d685eb555ede1860c1d5655042da
Artur <61751393+Arturyus92@users.noreply.github.com>
2024-02-26 15:15:40 +0300

    commit

--- migration_local.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source local.env
-
-sleep 2 && goose -dir "${MIGRATION_DIR}" postgres "${MIGRATION_DSN}" up -v
--- migration_prod.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source prod.env
-
-sleep 2 && goose -dir "${MIGRATION_DIR}" postgres "${MIGRATION_DSN}" up -v
